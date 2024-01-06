a28682352470e18d85751de7a7698c6c9534f8a3
Alberto Riffaud <alriffaud@google.com>
2024-01-06 08:02:39 -0800

    Creation of 10-binary_tree_depth.c file.

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - This function measures the depth of a binary tree.
+ * @tree: It's a pointer to the root node of the tree to delete.
+ *
+ * Return: The depth of a binary tree or 0 if tree is NULL.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree->parent == NULL)
+		return (0);
+	return (1 +  binary_tree_depth(tree->parent));
+}
