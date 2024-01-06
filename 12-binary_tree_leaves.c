fb86da6a89a0d29dc9ceec95aeb34dda8f82fcee
Alberto Riffaud <alriffaud@google.com>
2024-01-06 08:36:19 -0800

    Modification of 12-binary_tree_leaves.c file.

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - This function counts the leaves in a binary tree.
+ * @tree: It's a pointer to the root node of the tree to delete.
+ *
+ * Return: The number of leaves of the binary tree or 0 if tree is NULL.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
